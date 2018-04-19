98c23645940a4c0885cb7da01a3b69b8091805e3
Hasrokv Kodavieks <36292127+haskod@users.noreply.github.com>
2018-04-19 18:49:05 +0700

    Create twostones.cpp

---
+++ practice/twostones.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ll n;
+  scanf("%lld", &n);
+  
+  if (n % 2)
+    printf("Alice");
+  else
+    printf("Bob");
+  
+  printf("\n");
+  
+  return 0;
+}
